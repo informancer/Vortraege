531df93194809a947b134ded80b20ed664646abb
Adrien Beaucreux <informancer@web.de>
2012-05-01 17:25:22 +0200

    Added testing for the views

--- vortraege/tests.py
+++ vortraege/tests.py
@@ -6,11 +6,67 @@ Replace this with more appropriate tests for your application.
 """
 
 from django.test import TestCase
+from django.test.client import Client
 
+from vortraege.models import Vortrag
 
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
+from datetime import datetime
+from django.utils.timezone import utc, now
+
+class AllTestCase(TestCase):
+    def setUp(self):
+        # save a dummy vortrag
+        self.vortrag = Vortrag.objects.create(datum=now(),
+                                             thema='Cooles Vortrag',
+                                             referent='John Doe',
+                                             orgapate='Paul Smith',
+                                             beschreibung="""
+This is a very nice talk, presented by a guy who really knows what he's talking about. 
+The previous line should be wrapped in the text.
+""")
+        self.c = Client()
+                                             
+    def test_pressetext_attachment(self):
         """
-        Tests that 1 + 1 always equals 2.
+        Tests that the pressetext is an attachment
         """
-        self.assertEqual(1 + 1, 2)
+        response=self.c.get('/vortraege/%i/pressetext/'%self.vortrag.pk)
+        self.assertTrue(response['Content-Disposition'].startswith('attachment'))
+
+    def test_pressetext_wrapping(self):
+        response=self.c.get('/vortraege/%i/pressetext/'%self.vortrag.pk)
+        for line in response.content.split('\n'):
+            self.assertTrue(len(line) <= 70, 'The content should be wrapped at 70 characters')
+        
+    def test_aushang(self):
+        """
+        Tests that the pressetext is an attachment
+        """
+        response=self.c.get('/vortraege/%i/aushang/'%self.vortrag.pk)
+        self.assertTrue(response['Content-Disposition'].startswith('attachment'))
+        self.assertTrue(response['Content-Type'] == 'application/pdf')
+
+    def test_aushang_preview(self):
+        """
+        Tests that the pressetext is an attachment
+        """
+        response=self.c.get('/vortraege/%i/aushang/preview/'%self.vortrag.pk)
+        self.assertTrue(response['Content-Disposition'].startswith('attachment'))
+        self.assertTrue(response['Content-Type'].startswith('image/svg+xml'))
+
+    def test_flyer(self):
+        """
+        Tests that the pressetext is an attachment
+        """
+        response=self.c.get('/vortraege/%i/flyer/'%self.vortrag.pk)
+        self.assertTrue(response['Content-Disposition'].startswith('attachment'))
+        self.assertTrue(response['Content-Type'] == 'application/pdf')
+
+    def test_flyer_preview(self):
+        """
+        Tests that the pressetext is an attachment
+        """
+        response=self.c.get('/vortraege/%i/flyer/preview/'%self.vortrag.pk)
+        self.assertTrue(response['Content-Disposition'].startswith('attachment'))
+        self.assertTrue(response['Content-Type'].startswith('image/svg+xml'))
+
