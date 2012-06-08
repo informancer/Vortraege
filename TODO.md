bf1f1ec14e36923a61adb0d4f8426c0e0e82a9db
Adrien Beaucreux <informancer@web.de>
2012-06-08 19:43:43 +0200

    Refactoring of the QRCode filter to make it resizable and simpler to position.
    Refactor the qr code rendering code to make it positionable directly
    in the template (wrap the tag in a svg group with a translate
    transform attibute).  The parameters now indicate the size of the code
    and not its position.
    
    The resizing can also be made in the parent svg group element of the
    template, but I find it simpler to give the template filter a goal size
    and let it stretch the qrcode to fit it.
    
    This was achieved by using a custom factory to render the svg, which
    allowed us to remove the StringIO kludge, remove the namespace and add
    a method to resize.
    
    Updated test to reflech qrcode filter changes.

--- TODO.md
+++ TODO.md
@@ -4,7 +4,6 @@
  * Refactor the tests
  * Refactor the views
  * Extend the Admin Site Workflow for Aushaege
- * Add the QRCodes generation
  * Allow for uploaded files handling (PDF Documents)
  * Add events to flyers
  * Do not escape characters in the pressetext (html escape)
--- vortraege/templatetags/vortraege_extra.py
+++ vortraege/templatetags/vortraege_extra.py
@@ -4,7 +4,6 @@ from django.template.defaultfilters import stringfilter
 import qrcode
 import qrcode.image
 import qrcode.image.svg
-import StringIO
 import xml.etree.ElementTree
 from django.utils.safestring import mark_safe
 
@@ -12,6 +11,39 @@ from django.utils import timezone
 from icalendar import Event
 
 register = template.Library()
+
+class SvgGroup(qrcode.image.svg.SvgFragmentImage):
+    """SVG image builder
+
+    Creates a QR-code image as a SVG document fragment.
+    Ignores the {box_size} parameter, making the QR-code boxes
+    1mm square."""
+
+    def __init__(self, border, width, box_size):
+        super(SvgGroup, self).__init__(border, width, box_size)
+        self.kind = "SVG"
+        self._img = self._svg()
+
+    def _svg(self, tag = "g"):
+        return xml.etree.ElementTree.Element(tag)
+
+    def _rect(self, row, col, tag="rect"):
+        return xml.etree.ElementTree.Element(tag,
+                          x="%d" % (self.border + col),
+                          y="%d" % (self.border + row),
+                          width="1", height="1")
+
+    def _write(self, stream):
+        xml.etree.ElementTree.ElementTree(self._img).write(stream, xml_declaration=False)
+
+    def resize(self, size):
+        """resize the code by applying a scale transform"""
+        dimension = (2 * self.border + self.width)
+        factor = size / dimension
+        self._img.attrib['transform'] = 'scale(%f)' % factor
+
+    def get_tree(self):
+        return self._img
 
 @register.filter(name='vevent')
 def as_vevent(value):
@@ -29,19 +61,10 @@ def as_vevent(value):
 @stringfilter
 def render_qrcode(value, arg='0,0'):
     """Converts a string in a SVG QRCode, the argument being the place for the code."""
-    # The qrcode module tightly encapsulate its data.
-    # blocking changes to the generated svg,
-    # which is why we go he StringIO way.
-    temp_out = StringIO.StringIO()
     qr = qrcode.QRCode()
     qr.add_data(value)
-    img = qr.make_image(image_factory=qrcode.image.svg.SvgFragmentImage)
-    img.save(temp_out)
+    img = qr.make_image(image_factory=SvgGroup)
+    img.resize(150)
+    svg_tree = img.get_tree()
     
-    # Now change the XML so we can use it in the template
-    # ie: not a fragment.
-    e = xml.etree.ElementTree.fromstring(temp_out.getvalue())
-    e.tag = '{http://www.w3.org/2000/svg}g'
-    del e.attrib['version']
-    e.attrib['transform'] = 'translate(%s)'%arg
-    return mark_safe(xml.etree.ElementTree.tostring(e))
+    return mark_safe(xml.etree.ElementTree.tostring(svg_tree))
