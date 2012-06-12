07595cbc32b3673f6530deacae42118e4b057ed9
Adrien Beaucreux <informancer@web.de>
2012-06-12 23:20:01 +0200

    Updated dump_test_ref, which was broken by the views refactoring.

--- vortraege/management/commands/dump_test_ref.py
+++ vortraege/management/commands/dump_test_ref.py
@@ -1,12 +1,17 @@
 import codecs
 
+from django.test import Client
+from django.core.urlresolvers import reverse
+
 from django.core.management.base import BaseCommand, CommandError
 from vortraege.models import Talk
-from vortraege.views import render_svg_poster, render_svg_flyer
+#from vortraege.views import render_svg_poster, render_svg_flyer
 
 from django.db import (transaction, connection, connections, DEFAULT_DB_ALIAS,
     reset_queries)
 from django.core.management import call_command
+
+
 
 class Command(BaseCommand):
     args = '<fixture>'
@@ -17,13 +22,18 @@ class Command(BaseCommand):
         db = DEFAULT_DB_ALIAS 
         call_command('flush', verbosity=0, interactive=False, database=db)
         call_command('loaddata', *args, **{'verbosity': 0,  'database': db})
+        prefix = 'test/%s'%args[0].rstrip('.json')
+
+        client = Client()
 
         # Then create reference svgs for all Vortraege
         for talk in Talk.objects.all():
-            prefix = 'test/%s'%args[0].rstrip('.json')
-            expected = codecs.open('%s_poster_%s.svg'%(prefix, talk.pk), 'w', 'utf-8')
-            expected.write(render_svg_poster(talk))
-            expected = codecs.open('%s_flyer_%s.svg'%(prefix, talk.pk), 'w', 'utf-8')
-            expected.write(render_svg_flyer(talk))
+            expected = open('%s_poster_%s.svg'%(prefix, talk.pk), 'w')
+            response=client.get(reverse('vortraege_svg_poster', kwargs={'pk': 1}))
+            expected.write(response.content)
+            
+            expected = open('%s_flyer_%s.svg'%(prefix, talk.pk), 'w')
+            response=client.get(reverse('vortraege_svg_flyer', kwargs={'pk': 1}))
+            expected.write(response.content)
             
 
